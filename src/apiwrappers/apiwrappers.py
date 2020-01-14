193ddb9ba337c53fef2b40410b5f278b661613d9
Aleksei Maslakov <lesha.maslakov@gmail.com>
2020-01-14 14:49:01 +0300

    Fetch can be descriptor

--- src/apiwrappers/apiwrappers.py
+++ src/apiwrappers/apiwrappers.py
@@ -1,15 +1,41 @@
 import asyncio
-from typing import Awaitable, Callable, Generic, TypeVar, overload
+from typing import Awaitable, Callable, Generic, Optional, Type, TypeVar, overload
 
 from apiwrappers.entities import Request
-from apiwrappers.protocols import AsyncDriver, Driver
+from apiwrappers.protocols import AsyncDriver, Driver, WrapperLike
 
 T = TypeVar("T")
+RequestFactory = Callable[..., Request]
+RF = TypeVar("RF", bound=RequestFactory)
 
 
 class Fetch(Generic[T]):
     def __init__(self, factory: Callable[..., T]):
         self.factory: Callable[..., T] = factory
+        self.request_factory: Optional[RequestFactory] = None
+
+    @overload
+    def __get__(
+        self, obj: WrapperLike[Driver], objtype: Type[WrapperLike[Driver]]
+    ) -> Callable[..., T]:
+        ...
+
+    @overload
+    def __get__(
+        self, obj: WrapperLike[AsyncDriver], objtype: Type[WrapperLike[AsyncDriver]]
+    ) -> Callable[..., Awaitable[T]]:
+        ...
+
+    def __get__(self, obj, objtype):
+        def wrapper(*args, **kwargs):
+            request = self.request_factory(obj, *args, **kwargs)
+            return self.response(obj.driver, request)
+
+        return wrapper
+
+    def request(self, request_factory: RF) -> RF:
+        self.request_factory = request_factory
+        return request_factory
 
     @overload
     def response(self, driver: Driver, request: Request) -> T:
--- tests/test_apiwrappers.py
+++ tests/test_apiwrappers.py
@@ -1,15 +1,30 @@
 from dataclasses import dataclass
+from typing import Generic, TypeVar
 
 import pytest
 
-from apiwrappers import Fetch, Method, Request
+from apiwrappers import AsyncDriver, Driver, Fetch, Method, Request
 
 from . import factories
+
+DT = TypeVar("DT", Driver, AsyncDriver)
 
 
 @dataclass
 class User:
     user_id: int
+
+
+class APIWrapper(Generic[DT]):
+    user = Fetch(User)
+
+    def __init__(self, driver: DT):
+        self.driver: DT = driver
+
+    @user.request
+    def user_request(self, user_id: int) -> Request:
+        # pylint: disable=no-self-use
+        return Request(Method.GET, "https://example.com", f"/users/{user_id}")
 
 
 def test_fetch_response() -> None:
@@ -26,4 +41,21 @@ async def test_fetch_response_with_async_driver() -> None:
     driver = factories.make_async_driver(response)
     request = Request(Method.GET, "https://example.com", "/users/1")
     user = await Fetch(User).response(driver, request)
+    assert isinstance(user, User)
+
+
+def test_fetch_as_descriptor() -> None:
+    response = factories.make_response(b'{"user_id": 1}')
+    driver = factories.make_driver(response)
+    wrapper = APIWrapper(driver=driver)
+    user = wrapper.user(user_id=1)
+    assert isinstance(user, User)
+
+
+@pytest.mark.asyncio
+async def test_fetch_as_descriptor_with_async_driver() -> None:
+    response = factories.make_response(b'{"user_id": 1}')
+    driver = factories.make_async_driver(response)
+    wrapper = APIWrapper(driver=driver)
+    user = await wrapper.user(user_id=1)
     assert isinstance(user, User)
