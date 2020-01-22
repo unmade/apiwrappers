faad0f169843183d00625993722da4e65fa737c8
Aleksei Maslakov <lesha.maslakov@gmail.com>
2020-01-22 15:39:58 +0300

    Proof of concept for fromjson method

--- src/apiwrappers/utils.py
+++ src/apiwrappers/utils.py
@@ -1,8 +1,11 @@
+import dataclasses
 import os
 import urllib.parse
-from typing import Mapping, Optional
+from typing import Any, Mapping, Optional, TypeVar, Union, cast
 
 from apiwrappers.typedefs import JSON
+
+T = TypeVar("T")
 
 
 def build_url(host: str, path: str) -> str:
@@ -23,3 +26,49 @@ def getitem(data: JSON, key: Optional[str]) -> JSON:
         else:
             raise TypeError(f"Expected `Mapping` or `List`, got: {type(data)}")
     return data
+
+
+def fromjson(obj: T, data: JSON) -> T:
+    return cast(T, _fromjson(obj, data))
+
+
+def _fromjson(obj, data):
+    # pylint: disable=too-many-return-statements,too-many-branches
+    if dataclasses.is_dataclass(obj):
+        kwargs = {}
+        for field in dataclasses.fields(obj):
+            try:
+                kwargs[field.name] = _fromjson(field.type, data[field.name])
+            except KeyError:
+                if field.default is not dataclasses.MISSING:
+                    kwargs[field.name] = field.default
+                elif field.default_factory is not dataclasses.MISSING:
+                    kwargs[field.name] = field.default_factory()
+                else:
+                    raise
+        return obj(**kwargs)
+    if obj is Any:
+        return data
+    if hasattr(obj, "__origin__"):
+        origin = obj.__origin__
+        args = obj.__args__
+        if origin in (list, set, tuple):
+            if not isinstance(data, list):
+                raise ValueError(f"Expected value to be list, got: {type(data)}")
+            if origin is tuple and Ellipsis not in args:
+                return origin(_fromjson(tp, item) for tp, item in zip(args, data))
+            return origin(_fromjson(args[0], item) for item in data)
+        if origin is dict:
+            if not isinstance(data, Mapping):
+                raise ValueError(f"Expected value to be mapping, got: {type(data)}")
+            return origin(
+                (_fromjson(args[0], k), _fromjson(args[1], v)) for k, v in data.items()
+            )
+        if origin is Union:
+            if len(args) == 2 and isinstance(None, args[1]):
+                if data is None:
+                    return None
+                return _fromjson(args[0], data)
+            raise TypeError("Union is not supported")
+        raise TypeError("Abstract types is not supported")
+    return obj(data)
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,3 +1,7 @@
+from dataclasses import dataclass, field
+from decimal import Decimal
+from typing import Any, Dict, List, Mapping, Optional, Set, Tuple, Union
+
 import pytest
 
 from apiwrappers import utils
@@ -34,3 +38,194 @@ def test_getitem(data, key, expected):
 def test_getitem_raises_error():
     with pytest.raises(TypeError):
         utils.getitem("id", "id")
+
+
+def test_primitive_types_dataclass_fromjson() -> None:
+    @dataclass
+    class Item:
+        id: int
+        name: str
+        is_active: bool
+        price: float
+
+    item = utils.fromjson(
+        Item, {"id": "1", "name": "foo-1", "is_active": True, "price": "20.5"}
+    )
+    assert item == Item(id=1, name="foo-1", is_active=True, price=20.5)
+
+
+def test_complex_types_dataclass_fromjson() -> None:
+    @dataclass
+    class Author:
+        name: str
+
+    @dataclass
+    class Song:
+        duration: Decimal
+        author: Author
+
+    data = {
+        "duration": 8.5,
+        "author": {"name": "Unknown"},
+    }
+
+    song = utils.fromjson(Song, data)
+    assert song == Song(duration=Decimal(8.5), author=Author(name="Unknown"))
+
+
+@pytest.mark.parametrize("rating", [None, 10])
+def test_optional_dataclass_fromjson(rating) -> None:
+    @dataclass
+    class Album:
+        rating: Optional[int]
+
+    data = {"rating": rating}
+    album = utils.fromjson(Album, data)
+    assert album == Album(rating=rating)
+
+
+def test_union_dataclass_fromjson() -> None:
+    @dataclass
+    class Song:
+        duration: Union[int, float]
+
+    data = {"duration": 8}
+    with pytest.raises(TypeError) as excinfo:
+        utils.fromjson(Song, data)
+
+    assert str(excinfo.value) == "Union is not supported"
+
+
+def test_any_dataclass_fromjson() -> None:
+    @dataclass
+    class Album:
+        band: Any
+
+    data = {"band": {"title": "The Apiwrappers"}}
+
+    album = utils.fromjson(Album, data)
+    assert album == Album(band={"title": "The Apiwrappers"})
+
+
+def test_list_dataclass_fromjson() -> None:
+    @dataclass
+    class Song:
+        id: int
+
+    @dataclass
+    class Album:
+        songs: List[Song]
+
+    data = {
+        "id": 1,
+        "songs": [{"id": 1}, {"id": 2}],
+    }
+
+    album = utils.fromjson(Album, data)
+    assert album == Album(songs=[Song(1), Song(2)])
+
+
+def test_fix_length_tuple_dataclass_fromjson() -> None:
+    @dataclass
+    class Sudoku:
+        box_size: Tuple[int, str]
+
+    data = {"box_size": ["3", "3"]}
+    sudoku = utils.fromjson(Sudoku, data)
+    assert sudoku == Sudoku(box_size=(3, "3"))
+
+
+def test_variable_length_tuple_fromjson() -> None:
+    @dataclass
+    class Sudoku:
+        cells: Tuple[int, ...]
+
+    data = {"cells": ["1", "5", "3"]}
+    sudoku = utils.fromjson(Sudoku, data)
+    assert sudoku == Sudoku(cells=(1, 5, 3))
+
+
+def test_set_fromjson() -> None:
+    @dataclass
+    class UniqueFibonacci:
+        digits: Set[int]
+
+    data = {"digits": [1, 1, 3, 5, 8]}
+    fibonacci = utils.fromjson(UniqueFibonacci, data)
+    assert fibonacci == UniqueFibonacci(digits={1, 3, 5, 8})
+
+
+def test_list_from_not_list():
+    @dataclass
+    class Album:
+        songs: List[str]
+
+    data = {"songs": {"song-1": 2.5, "song-2": 3.5}}
+    with pytest.raises(ValueError) as excinfo:
+        utils.fromjson(Album, data)
+
+    assert str(excinfo.value) == "Expected value to be list, got: <class 'dict'>"
+
+
+def test_dict_fromjson() -> None:
+    @dataclass
+    class Config:
+        extras: Dict[str, str]
+
+    data = {"extras": {"persist": "1", "multithread": "0"}}
+    config = utils.fromjson(Config, data)
+    assert config == Config(extras={"persist": "1", "multithread": "0"})
+
+
+def test_dict_from_not_dict():
+    @dataclass
+    class Album:
+        songs: Dict[str, str]
+
+    data = {"songs": ["song-1", "song-2"]}
+    with pytest.raises(ValueError) as excinfo:
+        print(utils.fromjson(Album, data))
+
+    assert str(excinfo.value) == "Expected value to be mapping, got: <class 'list'>"
+
+
+def test_default_field_value_fromjson() -> None:
+    @dataclass
+    class Sudoku:
+        size: int = field(default=81)
+
+    data: Dict[str, Any] = {}
+    sudoku = utils.fromjson(Sudoku, data)
+    assert sudoku == Sudoku(size=81)
+
+
+def test_default_factory_field_value_fromjson() -> None:
+    @dataclass
+    class Sudoku:
+        cells: List[int] = field(default_factory=list)
+
+    data: Dict[str, Any] = {}
+    sudoku = utils.fromjson(Sudoku, data)
+    assert sudoku == Sudoku(cells=[])
+
+
+def test_no_default_field_value_fromjson() -> None:
+    @dataclass
+    class Sudoku:
+        size: int
+
+    data: Dict[str, Any] = {}
+    with pytest.raises(KeyError):
+        utils.fromjson(Sudoku, data)
+
+
+def test_abstract_types_fromjson() -> None:
+    @dataclass
+    class Config:
+        extras: Mapping[str, str]
+
+    data = {"extras": {"persist": "1", "multithread": "0"}}
+    with pytest.raises(TypeError) as excinfo:
+        utils.fromjson(Config, data)
+
+    assert str(excinfo.value) == "Abstract types is not supported"
