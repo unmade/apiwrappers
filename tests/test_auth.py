0b3942dd3b2914dd274ca095b77a71e41df65c43
abnerjacobsen <abner@apoana.com.br>
2021-12-28 19:20:11 -0300

    Add api key authentication mechanism

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -1,4 +1,4 @@
-from apiwrappers.auth import BasicAuth, TokenAuth
+from apiwrappers.auth import ApiKeyAuth, BasicAuth, TokenAuth
 
 
 def test_basic_auth_string_representation():
@@ -29,3 +29,18 @@ def test_token_auth_representation():
 def test_token_auth():
     auth = TokenAuth("dXNlcm5hbWU6cGFzc3dvcmQ=")
     assert auth() == {"Authorization": "Bearer dXNlcm5hbWU6cGFzc3dvcmQ="}
+
+
+def test_apikey_auth_string_representation():
+    auth = ApiKeyAuth(key="dXNlcm5hbWU6cGFzc3dvcmQ=", header="apikey")
+    assert str(auth) == "apikey dXNlcm5hbWU6cGFzc3dvcmQ="
+
+
+def test_apikey_auth_representation():
+    auth = ApiKeyAuth(key="dXNlcm5hbWU6cGFzc3dvcmQ=", header="apikey")
+    assert repr(auth) == "<ApiKeyAuth 'apikey ...'>"
+
+
+def test_apikey_auth():
+    auth = ApiKeyAuth(key="dXNlcm5hbWU6cGFzc3dvcmQ=", header="apikey")
+    assert auth() == {"apikey": "dXNlcm5hbWU6cGFzc3dvcmQ="}
